47d09f912402a121b9e741504df83e39bc99e778
marcdhi <gandhi.mardav@gmail.com>
2024-02-24 20:50:36 +0530

    server: one last test

--- server/app/main.py
+++ server/app/main.py
@@ -11,13 +11,13 @@ from app.api import endpoints
 app = FastAPI()
 
 # Add CORS middleware
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
